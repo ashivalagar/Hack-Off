4c318b1d69ac4cff475ba95a0a13c63ba813f647
Shiv Alagar <ashivalagar@gmail.com>
2020-02-08 22:33:08 +0800

    Updates

---
+++ precache-manifest.57afb16f0d6bac8285ccac9cd59b3488.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a19653b1978706012a5f183c0e96b9f",
+    "url": "/Hack-Off/index.html"
+  },
+  {
+    "revision": "a123393d38177219074d",
+    "url": "/Hack-Off/static/css/main.04f85144.chunk.css"
+  },
+  {
+    "revision": "fe9685e506b30cebfccf",
+    "url": "/Hack-Off/static/js/2.3a927a4c.chunk.js"
+  },
+  {
+    "revision": "fec65b0a15accb04ad706f209264222d",
+    "url": "/Hack-Off/static/js/2.3a927a4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a123393d38177219074d",
+    "url": "/Hack-Off/static/js/main.382064df.chunk.js"
+  },
+  {
+    "revision": "0a04211d58c3063ef78f",
+    "url": "/Hack-Off/static/js/runtime-main.7aeafce7.js"
+  },
+  {
+    "revision": "2f48abc8c45de9add264c5be432e878e",
+    "url": "/Hack-Off/static/media/Hacked-KerX.2f48abc8.ttf"
+  }
+]);
